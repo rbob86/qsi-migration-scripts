729f011e3209caadb4e3dfa85542e5069e40d72f
Ryan Bobrowski <ryan.bobrowski@gmail.com>
2024-08-13 10:51:05 -0400

    Updated lmanage parallel script

--- 2-lmanage-capturator/lmanage-parallel.py
+++ 2-lmanage-capturator/lmanage-parallel.py
@@ -1,12 +1,15 @@
 import os
 import subprocess
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import logging
 
 ini_files_dir = "../1-create-ini-files/ini-files"
 ini_files = sorted(os.listdir(ini_files_dir))[0:5]
 
 
-# Define a function to execute a single lmanage command
+logging.basicConfig(level=logging.INFO)
+
+
 def run_lmanage(ini_file):
     config_num = ini_file.split(".")[0]
     config_dir = f"./config/config-{config_num}"
@@ -21,18 +24,24 @@ def run_lmanage(ini_file):
         "--force",
     ]
 
-    process = subprocess.Popen(
-        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
-    )
+    logging.info(f"Running command: {' '.join(command)}")
 
-    # Print stdout and stderr in real-time
-    for line in process.stdout:
-        print(line, end="")
+    try:
+        process = subprocess.Popen(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
 
-    for line in process.stderr:
-        print(line, end="")
+        # Use communicate to get the output
+        stdout, stderr = process.communicate()
 
-    process.wait()
+        logging.info(stdout)
+        logging.error(stderr)
+
+        if process.returncode != 0:
+            logging.error(f"Error running {ini_file}: {stderr}")
+
+    except Exception as e:
+        logging.error(f"Exception occurred for {ini_file}: {e}")
 
     return f"Completed {ini_file}"
 
@@ -43,7 +52,8 @@ max_workers = 5  # Adjust this number based on your system
 
 # Run the lmanage commands in parallel
 with ThreadPoolExecutor(max_workers=max_workers) as executor:
-    futures = [executor.submit(run_lmanage, ini_file) for ini_file in ini_files]
+    futures = [executor.submit(run_lmanage, ini_file)
+               for ini_file in ini_files]
 
     for future in as_completed(futures):
         result = future.result()
